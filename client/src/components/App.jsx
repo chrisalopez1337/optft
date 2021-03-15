f5493d19e216d42a381f32c69e84a47a5d503d89
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-14 22:30:11 -0700

    Added basic styling for header

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,10 +5,16 @@ import styled from 'styled-components';
 // Children components
 import Header from './Header.jsx';
 
+// Styling
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
 export default function App() {
     return (
-        <>
+        <Container>
             <Header />
-        </>
+        </Container>
     );
 };
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,10 +3,24 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
+// Styling
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    border-bottom: 2px solid #e64545;
+    background-color: #1c1c1c;
+`;
+
+const Logo = styled.h2`
+    margin-left: 7px;
+    color: #e64545;
+`;
+
 export default function Header() {
     return (
-        <>
-            <h1>optft.gg</h1>
-        </>
+        <Container>
+            <Logo>optft.gg</Logo>
+        </Container>
     );
 }
