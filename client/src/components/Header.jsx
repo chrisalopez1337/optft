c8c28e886b77c564ad381279bf3fa2106d6ce9c0
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-14 22:54:08 -0700

    Added styling for header buttons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+// Children Components
+import HeaderButtons from './HeaderButtons.jsx';
 
 // Styling
 const Container = styled.div`
@@ -10,6 +12,13 @@ const Container = styled.div`
     flex-direction: row;
     border-bottom: 2px solid #e64545;
     background-color: #1c1c1c;
+`;
+
+const RightContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    margin-right: 10px;
 `;
 
 const Logo = styled.h2`
@@ -21,6 +30,9 @@ export default function Header() {
     return (
         <Container>
             <Logo>optft.gg</Logo>
+            <RightContainer>
+                <HeaderButtons />
+            </RightContainer>
         </Container>
     );
 }
---
+++ client/src/components/HeaderButtons.jsx
@@ -0,0 +1,38 @@
+// Dependencies
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+// Styling
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+`;
+
+const Button = styled.button`
+    border: 2px solid #e64545;
+    margin-right: 5px;
+    margin-left: 5px;
+    font-family: inherit;
+    padding: 7px;
+    font-size: 16px;
+    transition-duration: 0.2s;
+    color: whitesmoke;
+    font-weight: bold;
+    background-color: transparent;
+    border-radius: 7px;
+    &:hover {
+        background-color: #e64545;
+        border: 2px solid whitesmoke;
+    }
+`;
+
+export default function HeaderButtons() {
+    return (
+        <Container>
+            <Button>Sign Up</Button>
+            <Button>Log In</Button>
+        </Container>
+    );
+}
