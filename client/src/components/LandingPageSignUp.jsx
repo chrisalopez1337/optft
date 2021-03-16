724b7513b7d3797fd8535bd385935a5c43696dde
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-15 23:48:09 -0700

    Completed styling for landing page sign up form

--- client/src/components/LandingPageSignUp.jsx
+++ client/src/components/LandingPageSignUp.jsx
@@ -5,14 +5,15 @@ import styled from 'styled-components';
 // Styling
 const Container = styled.div`
     display: flex;
-    align-items: center;
     justify-content: flex-start;
+    align-items: center;
     flex-direction: column;
     margin: 15px;
     border: 2px solid #e64545;
     border-radius: 5px;
     padding: 15px;
     background-color: #1c1c1c;
+    min-width: 400px;
 `;
 
 const Form = styled.form`
@@ -30,11 +31,50 @@ const Input = styled.input`
    font-size: 14px;
    padding: 5px;
    color: whitesmoke;
+   margin-bottom: 15px;
+   min-width: 200px;
 `;
 
 const Label = styled.label`
     color: whitesmoke;
     font-weight: bold;
+    margin-bottom: 3px;
+`;
+
+const HasAccountButton = styled.button`
+    border: transparent;
+    margin-right: 5px;
+    margin-left: 5px;
+    font-family: inherit;
+    padding: 7px;
+    font-size: 16px;
+    transition-duration: 0.2s;
+    color: whitesmoke;
+    font-weight: bold;
+    background-color: transparent;
+    margin-top: 7px;
+    &:hover {
+        color: #e64545;
+    }
+`;
+
+const SignUpButton = styled.button`
+    border: 2px solid whitesmoke;
+    margin-right: 5px;
+    margin-left: 5px;
+    font-family: inherit;
+    padding: 7px;
+    font-size: 16px;
+    transition-duration: 0.2s;
+    color: whitesmoke;
+    font-weight: bold;
+    background-color: #e64545;
+    border-radius: 7px;
+    &:hover {
+        background-color: whitesmoke;
+        border: 2px solid #e64545;
+        color: #e64545;
+    }
 `;
 
 
@@ -54,6 +94,9 @@ export default function LandingPageSignUp() {
 
                 <Label htmlFor="passwordVerify">Verify Password</Label>
                 <Input type="password" name="passwordVerify" />
+
+                <SignUpButton>Create Account</SignUpButton>
+                <HasAccountButton>Already Signed Up?</HasAccountButton>
             </Form>
         </Container>
     );
