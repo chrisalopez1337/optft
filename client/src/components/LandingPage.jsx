c794e6ecb30b8b56b3a268185097d144dfc456bd
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-15 23:21:09 -0700

    Added landing page sign up component

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+// Children components
+import LandingPageSignUp from './LandingPageSignUp.jsx';
+
+// styling
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    width: 100%;
+`;
 
 export default function LandingPage() {
     return (
-        <>
-            <h1>Landing Page</h1>
-        </>
+        <Container>
+            <LandingPageSignUp />
+        </Container>
     );
 }
---
+++ client/src/components/LandingPageSignUp.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+export default function LandingPageSignUp() {
+    return (
+        <>
+            <h3>Landing Page Sign Up</h3>
+        </>
+    );
+}
