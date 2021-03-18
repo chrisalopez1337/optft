b9410a356199a9a82eb96bd1f56849214d97f8ef
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-17 22:51:37 -0700

    Added Icon for game info container to frame styling, as well as mode type and placement

--- client/src/components/InfoPageGameContainer.jsx
+++ client/src/components/InfoPageGameContainer.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
+import ossia from '../../dist/assets/test.png';
+
 // Styling
 const Container = styled.div`
     display: flex;
@@ -17,14 +19,50 @@ const Container = styled.div`
     transition-duration: 0.2s;
     &:hover {
         border: 2px solid whitesmoke;
-        background-color: #ff4d4d;
+        background-color: #402323;
     }
 `;
+
+const Row = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start; 
+    flex-direction: row;
+`;
+
+const RankTypeAndInfo = styled.div`
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    font-size: 16px;
+    flex-direction: column;
+    margin-left: 10px;
+`;
+
+const WonPlacementText = styled.p`
+    color: #10c20a;
+`;
+
+const Ossia = styled.img`
+    width: 30%;
+`;
+Ossia.defaultProps = {
+    src: ossia,
+}
+
 
 export default function InfoPageGameContainer() {
     return (
         <Container>
-            <h1>Game # Info</h1>
+            <h3>9 Months ago</h3>
+            <Row>
+                <Ossia />
+
+                <RankTypeAndInfo>
+                    <p>Ranked TFT</p>
+                    <WonPlacementText>1st place</WonPlacementText>
+                </RankTypeAndInfo>
+            </Row>
         </Container>
     );
 }
