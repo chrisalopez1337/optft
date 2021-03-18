d8003def0270166e24644ea162449cf28419f5a0
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-17 21:52:40 -0700

    Styled container for InfoPage and added the first sub-component InfoPageOverview

--- client/src/components/InfoPageGeneralStats.jsx
+++ client/src/components/InfoPageGeneralStats.jsx
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import InfoPageOverview from './InfoPageOverview.jsx';
+
+// Styling
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 10px;
+    border: 2px solid #e64545;
+    border-radius: 7px;
+    min-width: 800px;
+`;
 
 export default function InfoPageGeneralStats() {
     return (
-        <>
-            <p>General Stats</p>
-        </>
+        <Container>
+            <InfoPageOverview />
+        </Container>
     );
 }
---
+++ client/src/components/InfoPageOverview.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+export default function InfoPageOverview() {
+    return (
+        <>
+            <p>Overview</p>
+        </>
+    );
+}
