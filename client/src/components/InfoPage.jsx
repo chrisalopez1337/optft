4b4eebe834893cecc10998995fbf3d042e79e96e
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-17 21:36:22 -0700

    Added basic container for General info component and added a general stats component

--- client/src/components/InfoPage.jsx
+++ client/src/components/InfoPage.jsx
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+// Children components
+import InfoPageGeneralStats from './InfoPageGeneralStats.jsx';
 
 // Styling
 const Container = styled.div`
     display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 `;
 
 export default function InfoPage() {
     return (
         <Container>
-            <h1>Info Page</h1>
+            <h1>Summoner Name</h1>
+            <InfoPageGeneralStats />
         </Container>
     );
 }
