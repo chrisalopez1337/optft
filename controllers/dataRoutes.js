c6b6edb19693ec55cb3c83df56a34a83f26bc293
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-18 19:18:42 -0700

    Fixed some syntax errors in the route to retrieve all summoner information, fixed this and now the route works properly

--- controllers/dataRoutes.js
+++ controllers/dataRoutes.js
@@ -1,13 +1,18 @@
 const TftQuery = require('tft-query');
+// Global client settings
+const useRedis = true;
+const redisConfig = false;
 const apiKey = require('../config');
+
 module.exports = {
     getAllDataBySummonerName: async (req, res) => {
         try {
             const { summonerName } = req.params;
             const payload = { summonerName };
-            const config = { 'NA', payload, apiKey, true, false };
+            const region = 'NA';
+            const config = { region, payload, apiKey, useRedis, redisConfig };
             const client = new TftQuery(config);
-            const data = await getAllInfoBySummonerName();
+            const data = await client.getAllInfoBySummonerName();
             res.status(200).send(data);
         } catch(err) {
             console.log(err);
