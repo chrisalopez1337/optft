41f0f760a20cce6436a3beb2bf122db9f7c8ded4
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-22 17:33:45 -0700

    Added base phrases for generating the agro report

--- client/src/utils/recommendHandler.js
+++ client/src/utils/recommendHandler.js
@@ -14,6 +14,17 @@ class RecommendHandler {
             return 'Gold Left: On average you tend to have more gold left than your peers. Having spare gold is innevitable, but also can be a usefull resource. If you are about to die, make sure to use your gold for any extra chances!';
         }
     }
+
+    generateAgro() {
+        const { timeEliminated, lastRound, level } = this.differences;
+        let message;
+        const badStarter = 'Durability: You are often dropping out of the game before your peers, this can be due to a few reaons:';
+        const mediumStarter = 'Durability: Although you are close to your peers in your survival rate, we think you could still improve from these core concepts:';
+        const goodStarter = 'Durability: You tend to outlast most of your peers! Keep up the good work.';
+        if (timeEliminated < -5 && lastRound < -5 && level < 0) {
+            message = ''
+        }
+    }
 }
 
 module.exports = RecommendHandler;
