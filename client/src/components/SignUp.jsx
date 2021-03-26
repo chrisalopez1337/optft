f29787d79534da2baa47869576b99a6880f2f67a
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-25 21:09:13 -0700

    Added logic for sign up form, has complete field validation and submit is working as expected

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -84,25 +84,177 @@ const SignUpButton = styled.button`
     }
 `;
 
+
+
 export default function SignUp() {
+    
+       // Store form data;
+    const [fields, setFields] = useState({ username: '', password: '', verifyPassword: '', email: '' });
+    const { username, password, verifyPassword, email } = fields;
+
+    // Store user messaging
+    const [messages, setMessages] = useState({ submitMessage: '', usernameMessage: '', passwordMessage: '', verifyPasswordMessage: '', emailMessage: ''});
+    const { usernameMessage, passwordMessage, verifyPasswordMessage, emailMessage, submitMessage } = messages;
+
+    // Update handlers
+    function handleChange(e) {
+        const { target } = e;
+        const { value, name } = target;
+        setFields({...fields, [name]: value});
+    }
+
+    function handleMessage(name, value) {
+        setMessages({...messages, [name]: value});
+    }
+    // Form validation for username: 4-10 Chars no special.
+    useEffect(() => {
+        if (username === '') {
+            handleMessage('usernameMessage', '');
+        } else {
+            const regex = new RegExp("^[a-zA-Z0-9]{4,10}$");
+            if (regex.test(username)) {
+                // Make sure username doesnt already exist.
+                axios.get(`/api/users/${username}`)
+                    .then(({ data }) => {
+                        if (!data.username) {
+                            // Username is available.
+                            handleMessage('usernameMessage', '');
+                        } else {
+                            // Username is taken;
+                            const message = 'Username is already taken';
+                            handleMessage('usernameMessage', message);
+                        }
+                    })
+                    .catch(err => console.error(err));
+            } else {
+                const message = 'Username must be 4-10 characters, and contain no special characters.';
+                handleMessage('usernameMessage', message)
+            }
+        }
+    }, [username]);
+
+    // Form validation for password
+    useEffect(() => {
+        if (password === '') {
+            handleMessage('passwordMessage', '');
+        } else {
+            const regex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+            if (regex.test(password)) {
+                handleMessage('passwordMessage', '');
+            } else {
+                handleMessage('passwordMessage', 'Password must be eight or more characters, contain atleast one uppercase, one symbol, and one lowercase.');
+            }
+        }
+    }, [password]);
+
+    // Form validation for password verification
+    useEffect(() => {
+        if (verifyPassword === '' && password === '') {
+            handleMessage('verifyPasswordMessage', '');
+        } else if (verifyPassword === password) {
+            handleMessage('verifyPasswordMessage', '');
+        } else {
+            handleMessage('verifyPasswordMessage', 'Passwords must match');
+        }
+    }, [verifyPassword]);
+
+
+    // Form validation for email
+    useEffect(() => {
+        if (email === '') {
+            handleMessage('emailMessage', '');
+        } else {
+            const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+            if (regex.test(email.toLowerCase())) {
+                // First make sure the email hasnt already been registered.
+                axios.get(`/api/users/${email}`)
+                    .then(({ data }) => {
+                        if (!data.email) {
+                            // This means the email hasnt been taken.
+                            handleMessage('emailMessage', '');
+                        } else {
+                            // The email is already registered.
+                            const message = 'This email is already registered';
+                            handleMessage('emailMessage', message);
+                        }
+                    })
+                    .catch(err => console.error(err));
+            } else {
+                handleMessage('emailMessage', 'Please enter a valid email.');
+            }
+        }
+    }, [email]);
+
+
+    // Submit handler
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        let valid = true; // For some reason return statements werent working so this is a temp fix, will refactor later.
+        
+        // First make sure all the fields have been filled out.
+        const requiredFields = [username, email, password, verifyPassword];
+        for (let i = 0; i < requiredFields.length; i++) {
+            const item = requiredFields[i];
+            if (item === '') {
+                const message = 'All fields must be filled out';
+                handleMessage('submitMessage', message);
+                return;
+            }
+        }
+
+        // Then make sure the formatting is correct for all fields.
+        const errorMessages = [usernameMessage, emailMessage, passwordMessage, verifyPasswordMessage];
+        for (let i = 0; i < errorMessages.length; i++) {
+            const item = errorMessages[i];
+            if (item !== '') {
+                const message = 'Please make sure all fields are formatted correctly.';
+                handleMessage('submitMessage', message);
+                return;
+            }
+        }
+
+        // Format the data to be POSTed.
+        const userData = { username, email, password };
+        // Create the user.
+        axios.post('/api/users/create', userData)
+            .then(res => {
+                const message = `Thanks for signing up ${username}! Please log in :)`;
+                handleMessage('submitMessage', message);
+            })
+            .catch(err => console.error(err));
+    }
+
+
+
     return (
         <Container>
             <h3>Create an account</h3>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Label htmlFor="username">Username</Label>
-                <Input type="text" name="username" />
+                <Input type="text" name="username" value={username} onChange={handleChange} />
+                
+                { usernameMessage === '' ? null : <p>{usernameMessage}</p>}
 
                 <Label htmlFor="email">E-mail</Label>
-                <Input type="email" name="email" />
+                <Input type="email" name="email" value={email} onChange={handleChange} />
+
+                { emailMessage === '' ? null : <p>{emailMessage}</p>}
 
                 <Label htmlFor="password">Password</Label>
-                <Input type="password" name="password" />
+                <Input type="password" name="password" value={password} onChange={handleChange} />
 
-                <Label htmlFor="passwordVerify">Verify Password</Label>
-                <Input type="password" name="passwordVerify" />
+                { passwordMessage === '' ? null : <p>{passwordMessage}</p>}
 
-                <SignUpButton>Create Account</SignUpButton>
+                <Label htmlFor="verifyPassword">Verify Password</Label>
+                <Input type="password" name="verifyPassword" value={verifyPassword} onChange={handleChange}/>
+
+                { verifyPasswordMessage === '' ? null : <p>{verifyPasswordMessage}</p>}
+
+                <SignUpButton type="subimt">Create Account</SignUpButton>
                 <HasAccountButton>Already Signed Up?</HasAccountButton>
+                
+                { submitMessage === '' ? null : <p>{submitMessage}</p>}
             </Form>
         </Container>
     );
